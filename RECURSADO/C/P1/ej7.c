2923981edb5cf9a2f228b052d1b5389342c27fb9
Lorl <grassiantonella747@gmail.com>
2024-06-24 11:20:25 -0300

    ejercicio 7

--- RECURSADO/C/P1/ej7.c
+++ RECURSADO/C/P1/ej7.c
@@ -1,2 +1,29 @@
-// Ejercicio 7. Un año es bisiesto si es divisible por 4 y no es por 100, o si es divisible por 400.
-// Escriba un programa que lea un año y devuelva si es bisiesto o no.
+// Ejercicio 7. Un anio es bisiesto si es divisible por 4 y no es por 100, o si es divisible por 400.
+// Escriba un programa que lea un anio y devuelva si es bisiesto o no.
+
+#include <stdio.h>
+#include <assert.h>
+
+int bisiesto(int anio){
+    return (anio%4==0 && anio%100!=0) || (anio%400==0);
+}
+
+
+void test_bisiesto(void){
+    assert(bisiesto(2004)==1); //bisiesto
+    assert(bisiesto(2005)==0); // NO bisiesto
+    assert(bisiesto(1990)==0); //bisiesto
+}
+
+int main(){
+    int anio=0,resultado;
+    printf("Ingresa el anio de tu interés: ");
+    scanf("%d",&anio);
+    resultado=bisiesto(anio);
+    if (resultado){
+        printf("El anio %d es bisiesto",anio);
+    }else{printf("El anio %d NO es bisiesto",anio);}
+
+    test_bisiesto(); //tests
+    return 0;
+}
